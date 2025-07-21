7b0ccde06f0cd0ec6f9d15f061655a5fe7bd1cee
elianollivier <elian.ollivier@etu.unilim.fr>
2025-07-21 18:09:45 +0200

    modif constans

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   Constants._();
   static const String userAgent = 'FlutterIptvPlayer/1.0';
-  static const String supabaseUrl = 'https://YOUR_SUPABASE_URL';
-  static const String supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+  static const String supabaseUrl = 'https://xiyqvzkflgkycainmihd.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpeXF2emtmbGdreWNhaW5taWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2ODMyOTMsImV4cCI6MjA2ODI1OTI5M30.8LLCS-cVkZGhVLdRb-PHELQ-Vh6cjrEAfpLhGt78Es4';
 }
